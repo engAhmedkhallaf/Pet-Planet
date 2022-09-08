91ecac8ad6ea9b35f5909d912beb1dc6f2c5d1ac
Ahmed Khallaf <[ahmedkhallaf2098@gmail.com]>
2022-09-08 13:51:28 +0200

    Adding Firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:pet_planet/bloc_observer.dart';
 import 'package:pet_planet/core/app/app.dart';
@@ -11,6 +12,9 @@ void main() async {
   // Native Splash Screen
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // firebase initalize
+  await Firebase.initializeApp();
 
   // bloc observer
   Bloc.observer = MyBlocObserver();
